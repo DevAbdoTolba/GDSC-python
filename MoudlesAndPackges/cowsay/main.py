8430272c423eac02db6585108974558b232a2f79
Abdo Tolba <mtolba2004@gmail.com>
2023-03-07 18:18:51 +0200

    pyttsx3 And cowsay

---
+++ MoudlesAndPackges/cowsay/main.py
@@ -0,0 +1,3 @@
+import cowsay
+
+cowsay.cow("Hello, I am a cow!")
---
+++ MoudlesAndPackges/pyttsx3/main.py
@@ -0,0 +1,31 @@
+import pyttsx3
+
+# # sudo apt-get install espeak
+# engine = pyttsx3.init('espeak') # for windows use sapi5
+
+# engine.say("I am a bot trying to finish my first project")
+
+# engine.runAndWait()
+
+"""
+==============================//                 //==============================
+=============================//  another case   //===============================
+============================//                 //================================
+"""
+
+# engine = pyttsx3.init('espeak')
+# voices = engine.getProperty('voices')
+# print(len(voices))
+# start = input("start all? y/n")
+# if start == "y":
+#     for voice in voices:
+#         engine.setProperty('voice', voice.id)
+#         engine.say('Hello World!')
+# else :
+#     engine.setProperty('voice', voices[0].id)
+#     engine.say('Hello World!')
+
+# engine.runAndWait()
+
+
+
